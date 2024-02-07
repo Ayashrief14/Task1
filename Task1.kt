bb72fa982b253a2807f07dc1bf5d467a00300a82
Ayashrief14 <ayazaher14@gmail.com>
2024-02-07 23:43:06 +0200

    Task1

---
+++ Task1.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    val num = 5
+    var result = num
+    var a = 1
+    while (a < num) {
+
+        result *= a
+        a += 1
+
+    }
+    println("The factorial of  $num is $result" )
+
+}
